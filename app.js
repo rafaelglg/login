1c57ea5bd61cd3712744156c0955359452de5519
Rafael Loggiodice <rafael@Rafaels-MacBook-Pro.local>
2022-05-15 14:34:02 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require ("request");
+const https = require ("https");
+const { post } = require("request");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    
+    const data = {
+        members:[
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+
+    };
+
+    const jsonData = JSON.stringify(data);
+
+const url = "https://us12.api.mailchimp.com/3.0/lists/bad4ab4563";
+const options ={
+    method : "POST",
+    auth: "rafael1:bf8b3953f6b95bea5693a8b8b8305f19-us12"
+}
+
+
+const request = https.request(url,options,function(response){
+
+    if(response.statusCode ===200){
+        res.sendFile(__dirname + "/success.html");
+    } else {
+        res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data){
+        console.log(JSON.parse(data));
+    })
+})
+
+request.write(jsonData);
+request.end();
+});
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("servidor funcionando en el puerto 3000");
+})
+
+
+// API Key
+
+// bf8b3953f6b95bea5693a8b8b8305f19-us12
+
+// List ID
+
+// bad4ab4563
